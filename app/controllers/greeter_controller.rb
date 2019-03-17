2ede57a28c4e9008dc6c5ee9ddb8eaee7d5e79b0
Jonathan <38994980+jhnsolis@users.noreply.github.com>
2019-03-16 18:22:04 -0600

    Move business logic from view to controller

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,5 +1,12 @@
 class GreeterController < ApplicationController
   def hello
+    random_names = ["John", "Lucia", "Mike"]
+    @name = random_names.sample
+
+    @time = Time.now
+    
+    @times_displayed ||= 0 #Consider that these variables are not persistent between requests
+    @times_displayed += 1  #You need to store them in a DB or Session if you want to access them later
   end
   def goodbye
   end
