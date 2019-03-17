90461c593e52e1cddc873972219ddbb9ace34537
Jonathan <38994980+jhnsolis@users.noreply.github.com>
2019-03-16 18:05:14 -0600

    Add another way to define the routes: Controller#Action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'greeter/hello'
+  get 'greeter/hello'=> 'greeter#hello' #This means that when the browser gets a request greeter it will
+                                        #rout it to the greeter controller and hello action
   get 'greeter/goodbye'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
