b15863f254b349b770f8bec997aaf0b330ac7460
Jonathan <38994980+jhnsolis@users.noreply.github.com>
2019-03-16 23:03:22 -0600

    Clarify a comment set before

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'greeter/hello'=> 'greeter#hello' #This means that when the browser gets a request greeter it will
-                                        #rout it to the greeter controller and hello action
+                                        #route it to the greeter controller and hello action
+                                        #Use rake routes to see all the spcified routes
   get 'greeter/goodbye'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
