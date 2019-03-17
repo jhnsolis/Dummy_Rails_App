648490e3a468f82bdbffc2ee3cd25e2b419e05f8
Jonathan <38994980+jhnsolis@users.noreply.github.com>
2019-03-16 19:34:46 -0600

    Add helpers example

--- app/helpers/greeter_helper.rb
+++ app/helpers/greeter_helper.rb
@@ -1,2 +1,6 @@
 module GreeterHelper
+    def formatted_time(time) #This method is available for all views in the application
+        #Time string in AM/PM Format
+        time.strftime("%I:%M%p")
+    end
 end
