388a4427293b1d807b9268fe2eabd8610107ffa5
Jonathan <38994980+jhnsolis@users.noreply.github.com>
2019-04-01 20:57:16 -0600

    Add first set of tests

--- spec/countries_spec.rb
+++ spec/countries_spec.rb
@@ -1,1 +1,27 @@
+Capybara.default_driver = :selenium
+#Capybara.default_driver = :poltergeist
 
+Capybara.app_host = "https://jsolis-ruby-test.herokuapp.com/"
+
+describe "Countries" do
+    describe "visit_root" do
+        before { visit '/'}
+
+        it "Displays 'United Arab Emirates' (default)" do
+            expect(page).to have_content 'United Arab Emirates'
+        end
+
+        it "Displays table with 7 columns" do
+            expect(page).to have_selector(:xpath, "//table//tr[count(td)=7]")            
+        end
+
+        it "Column 1 should have the flag inside img tag" do
+            expect(page).to have_selector(:xpath, "//table//td[1]/img")
+        end
+
+        it "Displays the country 'South Africa' when looking for 'africa'" do
+            visit "?country=africa"
+            expect(page).to have_content 'South Africa'
+        end
+    end
+end
